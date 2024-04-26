dc46082270180fcb2b206c896bab6bdf984ee5fc
vaishnavi <vaishumurugaiah@gmail.com>
2024-04-26 19:53:57 +0530

    Initial commit

---
+++ login-signup/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+import LoginSignup from './Components/LoginSignupComponent/LoginSignup';
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  )
+}
+
+export default App;
